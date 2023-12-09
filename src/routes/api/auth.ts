60c06b21a35c668dd0d6372d11487a4bc85c9080
Mix Abcprintf <wh.cprintf@gmail.com>
2023-12-09 22:47:05 +0700

    Add user authentication to login endpoint

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -1,14 +1,48 @@
 import { Router, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const router = Router();
 
-router.post("/login", (req: Request, res: Response) => {
+router.post("/login", async (req: Request, res: Response) => {
   const { username, password } = req.body;
-  console.log(username, password);
 
-  res.json({
+  if (typeof username === "undefined") {
+    res.json({
+      status: `error`,
+      message: `username not found`,
+    });
+  }
+
+  if (typeof password === "undefined") {
+    res.json({
+      status: `error`,
+      message: `password not found`,
+    });
+  }
+
+  const user = await prisma.user.findFirst({
+    where: {
+      name: username,
+      password: password,
+    },
+  });
+
+  /**
+   * verify user
+   */
+  if (!user) {
+    res.status(401).json({
+      status: `error`,
+      message: `Login fail`,
+    });
+    return;
+  }
+
+  res.status(200).json({
     status: "success",
-    message: "auth successfully",
+    userInfo: user,
   });
 });
 
