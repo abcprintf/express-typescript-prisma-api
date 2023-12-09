e52d00e98d50f8319c461779273654e11fcf38d2
Mix Abcprintf <wh.cprintf@gmail.com>
2023-12-09 09:48:18 +0700

    Update

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,32 @@ dotenv.config();
 import morganMiddleware from "./config/morganMiddleware";
 import apiAuthRouters from "./routes/api/auth";
 import path from "path";
+import helmet from "helmet";
+import cors from "cors";
 
 const app: Express = express();
-const port = process.env.APP_PORT;
+const PORT = process.env.APP_PORT;
+const PUBLIC_PATH = path.join(__dirname, "../public");
+
+const getCorsOptions = () => {
+  if (process.env.NODE_ENV === "production") {
+    return {
+      origin: process.env.API_BASE_URL,
+    };
+  }
+  return undefined;
+};
+
+/**
+ * init security modules
+ */
+app.use(
+  helmet({
+    contentSecurityPolicy: true,
+  })
+);
+app.use(cors(getCorsOptions()));
+app.use(express.json());
 
 /**
  * init Logger
@@ -16,8 +39,13 @@ app.use(morganMiddleware);
 
 /**
  * static files
+ * Serve Static files including the react app static bundle
  */
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(
+  express.static(PUBLIC_PATH, {
+    maxAge: 30 * 24 * 60 * 3600,
+  })
+);
 
 /**
  * init routers
@@ -25,9 +53,16 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.use("/api/auth", apiAuthRouters);
 
 app.get("/", (req: Request, res: Response) => {
-  res.sendFile(__dirname + "/../public/index.html");
+  res.sendFile(`${PUBLIC_PATH}/index.html`);
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+/**
+ * not found handler
+ */
+app.use("*", (req: Request, res: Response) => {
+  res.status(404).send("Not Found");
+});
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
