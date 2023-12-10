ece75a13831ad732c9f2f4a12b40f2548ff7f426
Mix Abcprintf <wh.cprintf@gmail.com>
2023-12-10 21:16:55 +0700

    Refactor authentication routes and add signup functionality

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -3,18 +3,20 @@ import { PrismaClient } from "@prisma/client";
 import { jwtGenerate, jwtRefreshTokenGenerate } from "../../lib/jwt";
 import authMiddleware from "../../middleware/auth.middleware";
 import RequestWithUser from "../../interfaces/requestWithUser.interface";
+import Signup from "../../interfaces/auth/signup.interface";
+import { encryptPassword, comparePassword } from "../../lib/bcrypt";
 
 const prisma = new PrismaClient();
 
 const router = Router();
 
 router.post("/login", async (req: Request, res: Response) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
-  if (typeof username === "undefined") {
+  if (typeof email === "undefined") {
     res.json({
       status: `error`,
-      message: `username not found`,
+      message: `email not found`,
     });
   }
 
@@ -27,15 +29,24 @@ router.post("/login", async (req: Request, res: Response) => {
 
   const user = await prisma.user.findFirst({
     where: {
-      name: username,
-      password: password,
+      email: email,
     },
   });
 
   /**
    * verify user
+   * compare password
    */
   if (!user) {
+    res.status(401).json({
+      status: `error`,
+      message: `Login fail`,
+    });
+    return;
+  }
+
+  const valid = await comparePassword(password, user.password);
+  if (!valid) {
     res.status(401).json({
       status: `error`,
       message: `Login fail`,
@@ -58,6 +69,53 @@ router.post("/login", async (req: Request, res: Response) => {
     userInfo: user,
     access_token: token,
     refresh_token: refreshToken,
+  });
+});
+
+router.post("/signup", async (req: Request, res: Response) => {
+  const { name, email, password }: Signup = req.body;
+
+  if (typeof name === "undefined") {
+    res.json({
+      status: `error`,
+      message: `name not found`,
+    });
+  }
+
+  if (typeof email === "undefined") {
+    res.json({
+      status: `error`,
+      message: `email not found`,
+    });
+  }
+
+  if (typeof password === "undefined") {
+    res.json({
+      status: `error`,
+      message: `password not found`,
+    });
+  }
+
+  /**
+   * generate password hash
+   */
+  const passwordHash = await encryptPassword(password);
+
+  /**
+   * Prisma create user
+   */
+  const user = await prisma.user.create({
+    data: {
+      name: name,
+      email: email,
+      password: passwordHash,
+    },
+  });
+
+  res.status(200).json({
+    status: "success",
+    message: "signup successfully",
+    userInfo: user,
   });
 });
 
